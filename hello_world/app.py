7656214dbbba5bf4019c3fdff11b8ce9c90ed963
Vlad-Vargan <blach.smith.2000@gmail.com>
2021-02-03 16:28:28 +0200

    policy

--- hello_world/app.py
+++ hello_world/app.py
@@ -4,17 +4,12 @@ import uuid
 from urllib.parse import unquote_plus
 from PIL import Image
 
-
-s3_client = boto3.client(
-                's3',
-                aws_access_key_id=os.environ['access_key_id'],
-                aws_secret_access_key=os.environ['secret_access_key'],
-                region_name=os.environ['region']
-)
+s3_client = boto3.client('s3')
+SIZE = (304, 228)
 
 def resize_image(image_path, resized_path):
     with Image.open(image_path) as image:
-        image.thumbnail((304, 228))
+        image.thumbnail(SIZE)
         image.save(resized_path)
 
 def lambda_handler(event, context):
@@ -26,4 +21,4 @@ def lambda_handler(event, context):
         upload_path = '/tmp/resized-{}'.format(tmpkey)
         s3_client.download_file(bucket_name, key, download_path)
         resize_image(download_path, upload_path)
-        s3_client.upload_file(upload_path, bucket_name, f'thumbnails/resized-{key}')
+        s3_client.upload_file(upload_path, os.environ["EndBucketName"], key)
